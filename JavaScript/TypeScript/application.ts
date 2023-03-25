4707cbd64864a8ab8b5f51e92224a1211609afdd
Inès <inesecharoux@gmail.com>
2023-03-25 15:36:06 +0100

    Test

---
+++ JavaScript/TypeScript/application.ts
@@ -0,0 +1,27 @@
+﻿/// <reference path="Noise.ts" />
+
+/**
+ * Web application
+ */
+class Application
+{
+    
+    /**
+     * Main view
+     */
+    private _noise: Noise;
+
+    /**
+     * Constructor
+     */
+    constructor()
+    {
+        this._noise = new Noise();
+    }
+}
+
+/**
+ * Démarrage de l'application Web une fois la page chargée.
+ */
+var application: Application = null;
+$(window).ready(() => { application = new Application(); });
---
+++ JavaScript/application.js
@@ -0,0 +1,8 @@
+class Application {
+    constructor() {
+        this._noise = new Noise();
+    }
+}
+var application = null;
+$(window).ready(() => { application = new Application(); });
+//# sourceMappingURL=application.js.map
